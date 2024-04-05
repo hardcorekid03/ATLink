68ce41d9c7b43a19b5dc98c22883e4a4af4ae9e3
hardcorekid03 <155038197+hardcorekid03@users.noreply.github.com>
2024-04-05 08:20:50 +0800

    Update mobileview.js

--- js/mobileview.js
+++ js/mobileview.js
@@ -1,6 +1,25 @@
-const mobileMenuButton = document.getElementById("mobile-menu-button");
-const mobileMenu = document.querySelector(".mobile-menu");
+// const mobileMenuButton = document.getElementById("mobile-menu-button");
+// const mobileMenu = document.querySelector(".mobile-menu");
 
-mobileMenuButton.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
+// mobileMenuButton.addEventListener("click", () => {
+//   mobileMenu.classList.toggle("hidden");
+// });
+
+// Toggle mobile menu visibility
+document
+  .getElementById("mobile-menu-button")
+  .addEventListener("click", function () {
+    var mobileMenu = document.getElementById("mobile-menu");
+    if (mobileMenu.classList.contains("hidden")) {
+      mobileMenu.classList.remove("hidden");
+    } else {
+      mobileMenu.classList.add("hidden");
+    }
+  });
+// Close mobile menu when a menu item is clicked
+var mobileMenuItems = document.querySelectorAll("#mobile-menu a");
+mobileMenuItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    document.getElementById("mobile-menu").classList.add("hidden");
+  });
 });
