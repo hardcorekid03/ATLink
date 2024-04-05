b8d94a3ffe93f66d50fb1b2a04eac6c2628a072b
hardcorekid03 <parasapremiumko88@gmail.com>
2024-04-05 17:02:00 +0800

    update

--- js/animatedscroll.js
+++ js/animatedscroll.js
@@ -24,24 +24,3 @@ links.forEach(link => {
   });
 });
 });
-
-var glide01 = new Glide(".glide-01", {
-  type: "carousel",
-  focusAt: "center",
-  perView: 3,
-  autoplay: 3000,
-  animationDuration: 700,
-  gap: 24,
-  classes: {
-    activeNav: "[&>*]:bg-slate-700",
-  },
-  breakpoints: {
-    1024: {
-      perView: 2,
-    },
-    640: {
-      perView: 1,
-    },
-  },
-});
-glide01.mount();
